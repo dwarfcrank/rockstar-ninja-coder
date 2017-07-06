b7c26cb39089b8ed5de24dac2f02e8c6218efbf8
Matias Juntunen <matias.juntunen@gmail.com>
2017-07-06 22:38:06 +0300

    More developers and upgrades.

--- src/developers.ts
+++ src/developers.ts
@@ -28,6 +28,46 @@ export const developerTypes: { [devId: string]: DeveloperType } = {
         description: "Still unsullied by enterprise Java.",
         initialCost: 150,
         baseCommitRate: 3
+    },
+
+    swEngineer: {
+        rank: 3,
+        title: "Software Engineer",
+        description: "Knows the standard library and isn't afraid to use it.",
+        initialCost: 500,
+        baseCommitRate: 10
+    },
+
+    leetHaxor: {
+        rank: 4,
+        title: "1337 h4x0r",
+        description: "Eats bytecode for breakfast.",
+        initialCost: 1337,
+        baseCommitRate: 13.37
+    },
+
+    seniorSwEng: {
+        rank: 5,
+        title: "Senior Software Engineer",
+        description: "Actually designs their code before writing it.",
+        initialCost: 2000,
+        baseCommitRate: 18
+    },
+
+    cProgrammer: {
+        rank: 6,
+        title: "C Programmer",
+        description: "Uses the K&R book as a pillow.",
+        initialCost: 3000,
+        baseCommitRate: 30
+    },
+
+    rustacean: {
+        rank: 7,
+        title: "The Rustacean",
+        description: "Here to rewrite everything in Rust.",
+        initialCost: 5000,
+        baseCommitRate: 100
     }
 };
 
--- src/upgrades.ts
+++ src/upgrades.ts
@@ -38,6 +38,35 @@ export const upgrades: { [upgradeId: string]: Upgrade } = {
         }
     },
 
+    energyDrinks: {
+        title: "Energy Drinks",
+        description: "Fueled by caffeine and sugar, interns commit 10% faster.",
+        cost: 250,
+        modifiers: {
+            intern: {
+                multiplier: 0.10
+            }
+        },
+        requirements: {
+            intern: 20
+        }
+    },
+
+    zsh: {
+        title: "Zsh",
+        description: "Everything's better with a good shell. Overall productivity up 4%.",
+        cost: 750,
+        modifiers: {
+            all: {
+                additiveMultiplier: 0.04
+            }
+        },
+        requirements: {
+            intern: 15,
+            juniorSwEng: 15
+        }
+    },
+
     jetbrains: {
         title: "JetBrains Subscription",
         description: "IDEA is awesome. Junior SW Engineers' productivity up 4%",
@@ -67,6 +96,18 @@ export const upgrades: { [upgradeId: string]: Upgrade } = {
         }
     },
 
+    coffeeMachine: {
+        title: "Coffee Machine",
+        description: "A fancy machine to brew extra strong espresso. Total productivity up 5%.",
+        cost: 3000,
+        modifiers: {
+            all: {
+                additiveMultiplier: 0.05
+            }
+        },
+        requirements: {}
+    },
+
     extraTerminal: {
         title: "Extra Terminal Window",
         description: "Two windows, two shells, two commits! +1 commit per click.",
@@ -77,6 +118,88 @@ export const upgrades: { [upgradeId: string]: Upgrade } = {
             }
         },
         requirements: {}
+    },
+
+    crispr: {
+        title: "CRISPR",
+        description: "Use gene editing to grow a third arm. +50% commits per click.",
+        cost: 5000,
+        modifiers: {
+            player: {
+                multiplier: 1.50
+            }
+        },
+        requirements: {}
+    },
+
+    typeScript: {
+        title: "TypeScript",
+        description: "Static typing. 'Nuff said. Junior SW Engs' producitivty up 10%.",
+        cost: 2500,
+        modifiers: {
+            juniorSwEng: {
+                multiplier: 0.10
+            }
+        },
+        requirements: {
+            juniorSwEng: 25
+        }
+    },
+
+    c11: {
+        title: "C11",
+        description: "Bringing C to the 21st century. C programmers' productivity up 7%.",
+        cost: 10000,
+        modifiers: {
+            cProgrammer: {
+                multiplier: 0.07
+            }
+        },
+        requirements: {
+            cProgrammer: 10
+        }
+    },
+
+    cargo: {
+        title: "Cargo",
+        description: "Rusty crates filled with stuff. Rustaceans' productivity up 7%.",
+        cost: 15000,
+        modifiers: {
+            rustacen: {
+                multiplier: 0.07
+            }
+        },
+        requirements: {
+            rustacean: 10
+        }
+    },
+
+    scrum: {
+        title: "Scrum",
+        description: "Agile methods improve Software Engineers' productivity by 10%.",
+        cost: 10000,
+        modifiers: {
+            swEngineer: {
+                multiplier: 0.10
+            }
+        },
+        requirements: {
+            swEngineer: 10
+        }
+    },
+
+    greenFont: {
+        title: "Green Terminal Font",
+        description: "1337 h4x0rs look even more 1337 with a green font. 1337 h4x0rs' productivity up 15%.",
+        cost: 1337,
+        modifiers: {
+            leetHaxor: {
+                multiplier: 0.15
+            }
+        },
+        requirements: {
+            leetHaxor: 10
+        }
     }
 };
 
